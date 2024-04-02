da94d7cf0d8c0c0c61e88a91b63de3cef72eea5e
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2024-04-02 15:05:14 -0400

    Add sample cframe

--- dist/CDN-FILES.md
+++ dist/CDN-FILES.md
@@ -1,11 +1,14 @@
 # CDN Files
 
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/assets/logo.svg
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/index.js
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/index.min.js
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/index.min.js.map
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/style.css
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/style.min.css
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/style.min.css.map
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/template.html
-- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/template.min.html
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/index.js
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/index.min.js
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/index.min.js.map
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/assets/logo.svg
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/index.js
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/index.min.js
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/index.min.js.map
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/style.css
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/style.min.css
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/style.min.css.map
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/template.html
+- https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/template.min.html
---
+++ dist/sample-cframe/index.js
@@ -0,0 +1,98 @@
+fetch('https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/sample-cframe/template.html').then((response) => {
+	return response.text();
+}).then((templateString) => {
+	document.body.insertAdjacentHTML('beforeend', templateString);
+	const templateElement = document.body.lastElementChild;
+
+	customElements.define('sample-cframe', class extends HTMLElement {
+		static observedAttributes = [
+			'breadcrumbs',
+			'title'
+		];
+
+		#breadcrumbsElement;
+		#breadcrumbsColElement;
+		#titleElement;
+
+		constructor() {
+			super();
+
+			// console.log('CONSTRUCTOR');
+
+			this.attachShadow({ mode: 'open' });
+			this.shadowRoot.appendChild(templateElement.content.cloneNode(true));
+
+			this.#breadcrumbsElement = this.shadowRoot.querySelector('.breadcrumbs');
+			this.#breadcrumbsColElement = this.#breadcrumbsElement.querySelector('.container > .row > .col');
+			this.#titleElement = this.shadowRoot.querySelector('.title');
+		}
+
+		// connectedCallback() {
+		// 	console.group('CONNECTED CALLBACK');
+		// 	console.log('Custom element added to page.');
+		// 	console.groupEnd();
+		// }
+
+		// disconnectedCallback() {
+		// 	console.group('DISCONNECTED CALLBACK');
+		// 	console.log('Custom element removed from page.');
+		// 	console.groupEnd();
+		// }
+
+		// adoptedCallback() {
+		// 	console.group('ADOPTED CALLBACK');
+		// 	console.log('Custom element moved to new page.');
+		// 	console.groupEnd();
+		// }
+
+		attributeChangedCallback(name, oldValue, newValue) {
+			// console.group('ATTRIBUTE CHANGED CALLBACK');
+			// console.log('NAME', name);
+			// console.log('OLD VALUE', oldValue);
+			// console.log('NEW VALUE', newValue);
+			// console.groupEnd();
+
+			switch (name) {
+				case 'breadcrumbs':
+					if (!newValue) {
+						this.setBreadcrumbs(null);
+					} else {
+						this.setBreadcrumbs(JSON.parse(document.getElementById(newValue).textContent));
+					}
+					break;
+				case 'title':
+					if (!newValue) {
+						this.#titleElement.classList.add('d-none');
+						this.#titleElement.textContent = '';
+					} else {
+						this.#titleElement.textContent = newValue;
+						this.#titleElement.classList.remove('d-none');
+					}
+					break;
+			}
+		}
+
+		setBreadcrumbs(breadcrumbs) {
+			if (!breadcrumbs) {
+				this.#breadcrumbsElement.classList.add('d-none');
+				this.#breadcrumbsColElement.innerHTML = '';
+			} else {
+				this.#breadcrumbsElement.classList.remove('d-none');
+				const breadcrumbItems = breadcrumbs.map(({ text, link }) => {
+					return `<li class="breadcrumb-item"><a href="${link}">${text}</a></li>`;
+				});
+				this.#breadcrumbsColElement.innerHTML = `
+					<nav aria-label="breadcrumb">
+						<ol class="breadcrumb">
+							${breadcrumbItems.join('')}
+						</ol>
+					</nav>
+				`;
+			}
+		}
+
+		focus() {
+			this.#titleElement.focus();
+		}
+	});
+});
